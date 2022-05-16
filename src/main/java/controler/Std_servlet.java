01f973c578e0b0b2072ad8b64dcc11e3802c07ce
PHG <gkskfhem@naver.com>
2022-05-16 15:47:42 +0900

    index_selectAll href_modified and Std_servlet studentList add

--- src/main/java/controler/Std_servlet.java
+++ src/main/java/controler/Std_servlet.java
@@ -1,6 +1,7 @@
 package controler;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,39 +15,39 @@ import dto.StdDTO;
 
 @WebServlet("*.std")
 public class Std_servlet extends HttpServlet {
-	
-       
-    
-    public Std_servlet() {
-        super();
-        
-    }
 
-	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		response.setContentType("text/html;charset=UTF-8");
+		response.setCharacterEncoding("UTF-8");//한글 안깨지게
+		request.setCharacterEncoding("UTF-8");// post 방식 한글 안깨지게
+		
 		String uri = request.getRequestURI();
-		StdDAO dao = new StdDAO();
+		StdDAO dao = StdDAO.getInstance();
 		try {
-		if(uri.equals("/insert.std")) {
-			String name = request.getParameter("name");
-			int kor = Integer.parseInt(request.getParameter("kor"));
-			int eng = Integer.parseInt(request.getParameter("eng"));
-			int math = Integer.parseInt(request.getParameter("math"));
-			
-			int result = dao.insert(new StdDTO(0,0,name,kor,eng,math,null));
-			response.sendRedirect("/index.jsp");
-			
-		}
+			if(uri.equals("/insert.std")) {
+				String name = request.getParameter("name");
+				int kor = Integer.parseInt(request.getParameter("kor"));
+				int eng = Integer.parseInt(request.getParameter("eng"));
+				int math = Integer.parseInt(request.getParameter("math"));
+
+				int result = dao.insert(new StdDTO(0,0,name,kor,eng,math,null));
+				response.sendRedirect("/index.jsp");
+
+			}else if(uri.equals("/studentList.std")) {
+				List<stdDTO> list = dao.selectAll();
+				request.setAttribute("list", list);
+				request.getRequestDispatcher("/list/studentList.jsp").forward(request, response);	
+			}
 		}catch(Exception e) {
 			e.printStackTrace();
 			response.sendRedirect("error.jsp");
 		}
-		
+
 	}
 
-	
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
 	}
 
 }
