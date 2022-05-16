372b40175d5da43a6547f863530fc955e11ebf98
EH <wansohyun@naver.com>
2022-05-16 16:31:48 +0900

    오류잡기2

--- src/main/java/controler/Std_servlet.java
+++ src/main/java/controler/Std_servlet.java
@@ -15,42 +15,21 @@ import dto.StdDTO;
 
 @WebServlet("*.std")
 public class Std_servlet extends HttpServlet {
-	
-       
-    
-    public Std_servlet() {
-        super();
-        
-    }
 
-	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String uri = request.getRequestURI();
-		StdDAO dao = new StdDAO();
-		try {
-		if(uri.equals("/insert.std")) {
-			String name = request.getParameter("name");
-			int kor = Integer.parseInt(request.getParameter("kor"));
-			int eng = Integer.parseInt(request.getParameter("eng"));
-			int math = Integer.parseInt(request.getParameter("math"));
-			
-			int result = dao.insert(new StdDTO(0,0,name,kor,eng,math,null));
-			response.sendRedirect("/index.jsp");
-			
-		}else if(uri.equals("/delete.std")) {
-			
-			int sid = Integer.parseInt(request.getParameter("sid"));
-			
-			
-			int result = dao.delete(sid);
-			response.sendRedirect("/index.jsp");
-		}else if(uri.equals("/search.std")) {
-			
-			String name = request.getParameter("name");
-			List<StdDTO> list = dao.search(name);
-			request.setAttribute("list", list);
+		//		response.setContentType("text/html;charset=UTF-8");
+		response.setCharacterEncoding("UTF-8");//한글 안깨지게
+		request.setCharacterEncoding("UTF-8");// post 방식 한글 안깨지게
 
-<<<<<<< HEAD
+		String uri = request.getRequestURI();
+		StdDAO dao = StdDAO.getInstance();
+		try {
+			if(uri.equals("/insert.std")) {
+				String name = request.getParameter("name");
+				int kor = Integer.parseInt(request.getParameter("kor"));
+				int eng = Integer.parseInt(request.getParameter("eng"));
+				int math = Integer.parseInt(request.getParameter("math"));
+
 				int result = dao.insert(new StdDTO(0,0,name,kor,eng,math,null));
 				response.sendRedirect("/index.jsp");
 			}else if(uri.equals("/delete.std")) {
@@ -66,20 +45,15 @@ public class Std_servlet extends HttpServlet {
 				int result = dao.search(name);
 				response.sendRedirect("/search.jsp");
 			}
-=======
-			request.getRequestDispatcher("search.jsp").forward(request, response);
-		}
->>>>>>> a8303bd020452934ff3d6e30154121851a030f61
 		}catch(Exception e) {
 			e.printStackTrace();
 			response.sendRedirect("error.jsp");
 		}
-		
+
 	}
 
-	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
 	}
 
 }
